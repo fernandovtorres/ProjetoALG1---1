2e5b1ea8ce31a690815a622ff8a0a303651c9823
fernandovtorres <chereibos@gmail.com>
2024-09-15 17:59:56 -0300

    Brute force header

---
+++ libs/brute.h
@@ -0,0 +1,9 @@
+#ifndef BRUTE_H
+#define BRUTE_H
+#include "graph.h"
+typedef struct answer_ Answer;
+Answer *brute_force(Graph *graph, int start);
+void bestPath(Graph *graph, Answer **ans, int *currPath);
+int totalDistance(Graph *graph, int *currPath);
+
+#endif
--- libs/graph.h
+++ libs/graph.h
@@ -4,5 +4,6 @@
 #include <stdbool.h>
 typedef struct graph_ Graph;
 Graph *create_graph(int maxVertices, bool isDirected, bool isWeighted);
+int numberOfVertices(Graph *graph);
 bool insertEdge(Vertice *vert1, Vertice *vert2, int weight);
 #endif
