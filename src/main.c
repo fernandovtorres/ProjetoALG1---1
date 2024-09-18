a32bf5fc9e6b69bee9cceb06ecf113c130aa801b
fernandovtorres <chereibos@gmail.com>
2024-09-18 10:23:47 -0300

    grande aurichi

--- src/main.c
+++ src/main.c
@@ -1,13 +1,15 @@
 #include "../libs/branch.h"
 #include "../libs/brute.h"
+#include <math.h>
 #include <stdio.h>
+int pa(double n) { return ceil((n * (n + 1)) / 2); }
 
 int main(void) {
   int numberOfCities = 0, vert1 = 0, vert2 = 0, weight = 0, start = 0;
   scanf(" %d", &numberOfCities);
   scanf(" %d", &start);
   Graph *graph = create_graph(numberOfCities, 0, 1);
-  for (int i = 0; i < numberOfCities; i++) {
+  for (int i = 0; i < pa(numberOfCities - 1); i++) {
     scanf(" %d %d %d", &vert1, &vert2, &weight);
     insertEdge(graph, vert1, vert2, weight);
   }
